6bfa05f3e52cbacae4191e7ce093f4cc868e8a72
DawnGuo <c865835207@163.com>
2020-08-12 11:59:19 +0800

    Add the implementation of dynamic programming's several examples.
    1. 0-1 knapsack(basic && advance).
    2. double 11 shopping cart.
    3. YangHui triangle path minimum.
    4. matrix path minimum.
    5. coin change.
    
    By DawnGuo.

---
+++ dynamic/MatrixPathMin.java
@@ -0,0 +1,35 @@
+package dynamic;
+
+/**
+ * 矩阵路径求最短
+ */
+public class MatrixPathMin {
+    public int pathMin(int[][] matrix, int n) {
+        int[][] dp = new int[n][n];
+
+        // base case
+        dp[0][0] = matrix[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i-1] + matrix[0][i];
+        }
+        
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i-1][0] + matrix[i][0];
+        }
+
+        // 递归迭代
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + matrix[i][j];
+            }
+        }
+
+        return dp[n-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        MatrixPathMin mpm = new MatrixPathMin();
+        int[][] matrix = {{1, 3, 5, 9}, {2, 1, 3, 4}, {5, 2, 6, 7}, {6, 8, 4, 3}};
+        System.out.println(mpm.pathMin(matrix, 4));
+    }
+}
