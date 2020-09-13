cb9d019c54ba4539deb9b420b47416bf32d2aa94
DawnGuo <c865835207@163.com>
2020-09-13 21:37:54 +0800

    Add the modify of AC 1.0.
    144-PreorderTraversal.java
    94-InorderTraversal.java
    
    By DawnGuo.

--- LeetCodeRecord/ACFirst/144-PreorderTraversal.java
+++ LeetCodeRecord/ACFirst/144-PreorderTraversal.java
@@ -2,6 +2,7 @@ package ACFirst;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class PreorderTraversal {
     class TreeNode {
@@ -33,5 +34,61 @@ class PreorderTraversal {
         if (node.right != null) {
             getPre(node.right, res);
         }
+    }
+
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        if (root == null) {
+            return res;
+        }
+
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        
+        return res;
+    }
+
+    public List<Integer> preorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        
+        TreeNode curr = root;
+        TreeNode pre = null;
+
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right;     
+            } else {
+                pre = curr.left;
+                while (pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+
+                if (pre.right ==  null) {
+                    res.add(curr.val);
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    pre.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+
+        return res;
     }
 }
--- LeetCodeRecord/ACFirst/94-InorderTraversal.java
+++ LeetCodeRecord/ACFirst/94-InorderTraversal.java
@@ -2,6 +2,7 @@ package ACFirst;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class InorderTraversal {
 
@@ -35,5 +36,53 @@ class InorderTraversal {
         if (node.right != null) {
             getInorder(node.right, res);
         }
+    }
+
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        
+        TreeNode curr = root;
+
+        while (curr != null || !stack.empty()) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                TreeNode node = stack.pop();
+                res.add(node.val);
+                curr = node.right;
+            }
+        }
+
+        return res;
+    }
+
+    public List<Integer> inorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        TreeNode curr = root;
+        TreeNode pre = null;
+
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right;
+            } else {
+                pre = curr.left;
+                while (pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    pre.right = null;
+                    res.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+
+        return res;
     }
 }
