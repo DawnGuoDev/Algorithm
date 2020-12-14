2ffc1c84fce9c84def8f7d8a622a560d1200a889
DawnGuo <c865835207@163.com>
2020-12-14 21:37:00 +0800

    Modify the method 1.0 of LeetCode.
    offer65-Add.java
    
    By DawnGuo.

--- LeetCodeRecord/MethodFirst/offer65-Add.java
+++ LeetCodeRecord/MethodFirst/offer65-Add.java
@@ -3,7 +3,7 @@ package MethodFirst;
 class Add {
     public int add(int a, int b) {
         while (b != 0) {
-            int c = (a & b) >> 1;
+            int c = (a & b) << 1;
             a ^= b;
             b = c;
         }
