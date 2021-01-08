987e37bee17daa7ab4a972f3ed5b0140c94f5bfd
DawnGuo-MBP <c865835207@163.com>
2021-01-08 22:30:21 +0800

    Modify the method 1.0 of LeetCode.
    118-Generate.go
    
    By DawnGuo.

--- LeetCodeRecord/MethodFirst-Golang/118-Generate.go
+++ LeetCodeRecord/MethodFirst-Golang/118-Generate.go
@@ -5,8 +5,8 @@ func generate(numRows int) [][]int {
 
 	for i := 0; i < numRows; i++ {
 		temp := make([]int, i+1)
-		temp[0] = 0
-		temp[i] = 0
+		temp[0] = 1
+		temp[i] = 1
 
 		for j := 1; j < i; j++ {
 			temp[j] = res[i-1][j-1] + res[i-1][j]
